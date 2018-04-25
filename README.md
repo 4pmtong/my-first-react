7379b9d7948f5153af935a72bcb4758757df9842
chen_tong <chentong@zuzuche.com>
2018-04-25 16:06:07 +0800

    :tata: init

---
+++ README.md
@@ -0,0 +1,16 @@
+<!--
+  -- --------------------------------------------------------
+  -- @file readme.md
+  -- @author chentong <chentong@zuzuche.com>
+  -- @date 2018-04-25 15:45:12
+  -- @last_modified_by chentong <chentong@zuzuche.com>
+  -- @last_modified_date 2018-04-25 15:46:47
+  -- @copyright (c) 2018 @fe/my-first-react
+  -- --------------------------------------------------------
+ -->
+
+# my first react
+
+## A react test
+
+> just a test
---
+++ src/main.js
@@ -0,0 +1,30 @@
+/**
+ ** ********************************************************
+ ** @file main.js
+ ** @author chentong <chentong@zuzuche.com>
+ ** @date 2018-04-25 15:56:15
+ ** @last_modified_by chentong <chentong@zuzuche.com>
+ ** @last_modified_date 2018-04-25 15:57:19
+ ** @copyright (c) 2018 @fe/my-first-react
+ ** ********************************************************
+ */
+
+ var data = {
+    "发票标题": "平安科技",
+    "发票代码": "dddddddd",
+    "发票号码": "5555555",
+    "开票日期": "11111",
+    "机器编号": "4444444"
+};
+
+function initDetailsInfo(obj){
+    const tempArray = [];
+    Object.keys(obj).forEach((key, index) => {
+    tempArray.push({ name: key, value: obj[key], id: index });
+    console.log(index, key);
+    });
+    console.log(tempArray, 'temparr');
+    return tempArray;
+}
+
+initDetailsInfo(data);
